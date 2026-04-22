e418b06c93018c54645d3ff39fc5f3953a026eee
Luke Hickey <lukehickey08@gmail.com>
2026-04-22 16:14:26 -0700

    added auto investment split

--- splitter.py
+++ splitter.py
@@ -6,10 +6,24 @@ splits = {
     'Travel': 0.05
 }
 
+investments = {
+    'VOO': 0.60,
+    'QQQM': 0.30,
+    'SCHD': 0.10
+}
+
 paycheck = float(input("Enter the amount to split: $"))
+investment_amount = paycheck * 0.20
 
 print("\n--- Paycheck Breakdown ---\n")
 
 for category, percent in splits.items():
     amount = paycheck * percent
     print(f'{category}: ${amount:.2f}')
+
+
+print("\n--- Investing Breakdown ---\n")
+
+for stock, percent in investments.items():
+    inv = investment_amount * percent
+    print(f'{stock}: ${inv}')
